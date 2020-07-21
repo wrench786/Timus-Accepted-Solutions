1a05a70b2ae6eb29165777585fc8309de34e4af3
Md. Minhajul Islam <66138525+wrench786@users.noreply.github.com>
2020-07-21 23:03:13 +0600

    Added 2012. About Grisha N.cpp
    Problem Link: https://acm.timus.ru/problem.aspx?space=1&num=2012

---
+++ 2012. About Grisha N.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int f;
+    cin>>f;
+    if(12-f<=5){
+        cout<<"YES"<<endl;
+    }
+    else{
+        cout<<"NO"<<endl;
+    }
+}
